0fe370065d26903b62cfe3c26b27c16442edcd1e
Ashutosh Tharu <chaudharyashustosh102@gmail.com>
2026-01-28 07:46:19 +0545

    utils: update __init__ to export utility functions

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,1 +1,13 @@
 """Utility functions for SecureAttend."""
+
+from app.utils.validators import validate_student_id, validate_subject
+from app.utils.formatters import format_timestamp, format_student_id
+from app.utils.logging_config import setup_logging
+
+__all__ = [
+    'validate_student_id',
+    'validate_subject',
+    'format_timestamp',
+    'format_student_id',
+    'setup_logging'
+]
