bd15655a88bc6920dfd41f5ca5805f60157c177d
Ashutosh Tharu <chaudharyashustosh102@gmail.com>
2026-01-28 07:44:34 +0545

    crypto: update __init__ to export exception classes

--- app/crypto/__init__.py
+++ app/crypto/__init__.py
@@ -2,5 +2,15 @@
 
 from app.crypto.qr_generator import generate_signed_qr
 from app.crypto.qr_verifier import verify_qr_signature
+from app.crypto.keys import KeyManager
+from app.crypto.exceptions import CryptoError, KeyGenerationError, SignatureError, VerificationError
 
-__all__ = ['generate_signed_qr', 'verify_qr_signature']
+__all__ = [
+    'generate_signed_qr',
+    'verify_qr_signature',
+    'KeyManager',
+    'CryptoError',
+    'KeyGenerationError',
+    'SignatureError',
+    'VerificationError'
+]
